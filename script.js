3b7c0387878840e9f490ece692e1afaf274d67d2
vikass18 <kumarvikas53469@gmail.com>
2025-12-08 10:46:43 +0530

    Custom Cursor

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-var card = document.querySelector("#card");
+var main = document.querySelector("#main");
+var cursor = document.querySelector(".cursor");
 
-var heartIcon = document.querySelector("#love");
-
-card.addEventListener("dblclick", function () {
-  heartIcon.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)'
-  heartIcon.style.opacity = '0.8';
-
-  setTimeout(() => {
-    heartIcon.style.transform = 'translate(-50%, -300%) scale(1.2) rotate(60deg)'
-  } ,800);
-
-  setTimeout(() => {
-    heartIcon.style.opacity = '0';
-}, 1000);
-
-  setTimeout(() => {
-  heartIcon.style.transform = 'translate(-50%, -50%) scale(0) rotate(-60deg)'  
-}, 1200);
-
+main.addEventListener("mousemove", (dets) => {
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
 });
-
