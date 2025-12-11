749e41716834c31bfd1a8b586c859e22bceab043
vikass18 <kumarvikas53469@gmail.com>
2025-12-11 19:22:28 +0530

    Spotlight Effect

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-var main = document.querySelector("#main");
-var cursor = document.querySelector(".cursor");
-
-main.addEventListener("mousemove", (dets) => {
-  cursor.style.left = dets.x + "px";
-  cursor.style.top = dets.y + "px";
+addEventListener("mousemove", function (event) {
+  console.log(event.clientX, event.clientY);
+  document.querySelector(".box").style.setProperty("--x", event.clientX + "px");
+  document.querySelector(".box").style.setProperty("--y", event.clientY + "px");
 });
