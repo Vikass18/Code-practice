1ecc9987be39a09bdc0e1b4759bbfe96f503496c
vikass18 <kumarvikas53469@gmail.com>
2025-12-13 21:08:36 +0530

    card flex

--- script.js
+++
@@ -1,5 +0,0 @@
-addEventListener("mousemove", function (event) {
-  console.log(event.clientX, event.clientY);
-  document.querySelector(".box").style.setProperty("--x", event.clientX + "px");
-  document.querySelector(".box").style.setProperty("--y", event.clientY + "px");
-});
