c3959329fc382b5d6bd2177b508b0fbcced43343
vikass18 <kumarvikas53469@gmail.com>
2025-12-25 22:34:15 +0530

    react project

---
+++ React Project/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="image-div">
+        <img
+          className="image"
+          src="https://media.istockphoto.com/id/1072608290/photo/girl-practice-tennis-with-coach.jpg?s=612x612&w=0&k=20&c=1jSjJoSL10teayZPlRWvbgDR0l22br3g_HZBLFY1BR8="
+        ></img>
+        <div className="text">
+          <h1>Unleash Your Inner Champion Today. <br></br>All In One Place.</h1>
+          <h3>Join the ultimate tennis experience where passion meets performance, and every swing brings you closer to victory.</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
